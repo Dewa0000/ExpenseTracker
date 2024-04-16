4c2de3333839cdb3e3d2eed2468d70ed1a77d890
Dewa0000 <dewashishsahu8888@gmail.com>
2024-04-16 14:00:07 +0530

    expense

---
+++ random-Qoute/src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css';
+
+export const Header = () => {
+    return (
+        
+        <div className="body">
+            <h2 className="title">Expense Tracker</h2>
+        </div>
+        
+    )
+}
